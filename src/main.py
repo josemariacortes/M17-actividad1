27ccda0c8af25070574e34105ff2675c39f87270
josemariacortes <josemaria@cortesdiaz.com>
2026-06-02 20:31:56 +0200

    revert el commit: Cambiamos el mensaje de salida

--- src/main.py
+++ src/main.py
@@ -7,5 +7,5 @@ def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'El lanzamiento {idx+1} ha obtenido el resultado: {result}')
+    print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
